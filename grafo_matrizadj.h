53c82f3056fbcb2d97c68ebe9fb012eb8b3f631f
Jeniffer Martins <martins.jeniffer@usp.br>
2021-04-24 22:28:00 -0300

    CREATED: verticeValido, insereAresta, existeAresta, removeAresta, listaAdjVazia, proxListaAdj

--- grafo_matrizadj.h
+++ grafo_matrizadj.h
@@ -2,6 +2,7 @@
 
 #define MAXNUMVERTICES 100
 #define AN -1 /* Aresta nula */
+#define VERTICE_INVALIDO -1 /* numero de vertice invalido ou ausente */
 
 typedef int Peso;
 typedef struct {
@@ -18,4 +19,52 @@ typedef struct {
  * Vertices vao de 1 a nv
  * Retorna true se inicializou com sucesso e false caso contrário
  */
-bool inicializaGrafo(Grafo* grafo, int nv);
+bool inicializaGrafo(Grafo * grafo, int nv);
+
+/*
+ * bool verticeValido(Grafo * grafo, int vertice):
+ * 
+ * Verifica que o numero do vertice eh valido no grafo.
+ */
+bool verticeValido(Grafo * grafo, int vertice);
+
+/*
+ * void insereAresta(Grafo * grafo, int v1, int v2, Peso peso):
+ * 
+ * Insere a aresta entre o vértice 1 (v1) e o vértice 2 (v2).
+ */
+void insereAresta(Grafo * grafo, int v1, int v2, Peso peso);
+
+/*
+ * bool existeAresta(Grafo * grafo, int v1, int v2):
+ * 
+ * Retorna true se existe a aresta e false caso contrário
+ */
+bool existeAresta(Grafo * grafo, int v1, int v2);
+
+/*
+ * bool removeAresta(Grafo * grafo, int v1, int v2):
+ * 
+ * Remove a aresta (v1, v2) do grafo colocando AN em sua celula
+ * Se a aresta existia, coloca o peso dessa aresta em "peso" e retorna true
+ * caso contrario, retorna false e "peso" é inalterado
+ */
+bool removeAresta(Grafo * grafo, int v1, int v2, Peso * peso);
+
+/*
+ * bool listaAdjVazia(Grafo * grafo, int v):
+ * 
+ * Retorna true se a lista de adjacentes de um dado vertice eh vazia
+ * e false caso contrario
+ */
+bool listaAdjVazia(Grafo * grafo, int vertice);
+
+/*
+ * int proxListaAdj(Grafo * grafo, int vertice, int atual):
+ * 
+ * Trata-se de um iterador sobre a lista de adjacencia do vertice
+ * Retorna o proximo vertice adjacente a v, partindo do vertice "atual"
+ * adjacente a v ou -1 se a lista de adjacencia tiver terminado
+ * sem um novo proximo
+ */
+int proxListaAdj(Grafo * grafo, int vertice, int atual);
