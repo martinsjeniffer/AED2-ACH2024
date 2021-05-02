d8dc19f25eedb22513b10944c970eec04e7a54be
Jeniffer Martins <martins.jeniffer@usp.br>
2021-05-02 16:14:19 -0300

    FEAT: função main da implementacao por lista de Adjacencias

--- ListaAdj/testa_grafo.c
+++ ListaAdj/testa_grafo.c
@@ -2,5 +2,13 @@
 #include "grafo_listaadj.h"
 
 int main() {
+    Grafo g1;
+    int numVertices;
+
+    do {
+        printf("Digite o número de vertices do grafo\n");
+        scanf("%d", &numVertices);
+    } while (!inicializaGrafo(&g1, numVertices));
+
     return 0;
 }
