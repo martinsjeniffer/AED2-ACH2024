4d77ea5a18548daea7f03ef558f0fee467de259c
Jeniffer Martins <martins.jeniffer@usp.br>
2021-05-26 22:49:11 -0300

    Identação

--- EP1/conectividade.c
+++ EP1/conectividade.c
@@ -2,36 +2,36 @@
 #include "grafo_listaadj.h"
 
 void leGrafo(FILE * arquivoGrafo, Grafo* grafo) {
-    int nVertices, nArestas;
-    int v1, v2;
-    Peso peso;
+  int nVertices, nArestas;
+  int v1, v2;
+  Peso peso;
 
-    if(arquivoGrafo == NULL) {
-        fprintf(stderr, "O arquivo não existe!");
-        return;
-    }
-
-    if (!fscanf(arquivoGrafo, "%d %d", &nVertices, &nArestas)) {
-        fprintf(stderr, "Problemas ao ler nVertices e nArestas");
-        return;
-    }
-
-    inicializaGrafo(grafo, nVertices);
-
-    while(fscanf(arquivoGrafo, "%d %d %d", &v1, &v2, &peso) != EOF) {
-        insereAresta(grafo, v1, v2, peso);
-    }
-
-    fclose(arquivoGrafo);
+  if(arquivoGrafo == NULL) {
+    fprintf(stderr, "O arquivo não existe!");
     return;
+  }
+
+  if (!fscanf(arquivoGrafo, "%d %d", &nVertices, &nArestas)) {
+    fprintf(stderr, "Problemas ao ler nVertices e nArestas");
+    return;
+  }
+
+  inicializaGrafo(grafo, nVertices);
+
+  while(fscanf(arquivoGrafo, "%d %d %d", &v1, &v2, &peso) != EOF) {
+    insereAresta(grafo, v1, v2, peso);
+  }
+
+  fclose(arquivoGrafo);
+  return;
 }
 
 int main() {
-    Grafo grafo;
+  Grafo grafo;
 
-    if(stdin) leGrafo(stdin, &grafo);
-    imprimeGrafo(&grafo);
-    liberaGrafo(&grafo);
+  if(stdin) leGrafo(stdin, &grafo);
+  imprimeGrafo(&grafo);
+  liberaGrafo(&grafo);
 
-    return 0;
+  return 0;
 }
