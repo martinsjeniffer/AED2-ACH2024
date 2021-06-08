f39b2459f050bff066db0ab089985baf7bfa3218
Jeniffer Martins <martins.jeniffer@usp.br>
2021-06-08 00:43:16 -0300

    ex de chamada com valgrind

--- EP1/conectividade.c
+++ EP1/conectividade.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "grafo_listaadj.h"
-/* ex de chamada: ./grafo.exe < entrada.txt > saida.txt 2> erro.txt */
+/* ex de chamada: valgrind ./grafo.exe < entrada.txt > saida.txt 2> erro.txt */
 
 void leGrafo(FILE * arquivoGrafo, Grafo* grafo) {
   int nVertices, nArestas;
